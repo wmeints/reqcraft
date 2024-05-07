327adcc92e54cdce93d1317feee2d05393da01d2
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-05-07 09:33:36 +0200

    Add volume storage for the qdrant database

--- host/Reqcraft.AppHost/Program.cs
+++ host/Reqcraft.AppHost/Program.cs
@@ -2,31 +2,24 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-// Always parameterize secrets! This is a good practice to follow, even if you're not using the Azure Developer CLI.
 var vectorDatabaseApiKey = builder.AddParameter("VectorDatabaseApiKey", secret: true);
 var postgresDatabasePassword = builder.AddParameter("PostgresDatabasePassword", secret: true);
 
-// Locally we use a connection string to the Azure Application Insights resource, but in production we use the Azure
-// Application Insights resource, so we can deploy through the Azure Developer CLI automatically.
 var applicationInsights = builder.ExecutionContext.IsPublishMode ? 
     builder.AddAzureApplicationInsights("app-insights") : 
     builder.AddConnectionString("app-insights");
 
-// Store the data for the SQL Server on the host. This makes it possible to continue debugging without
-// having to repeat setup steps. The init mount is especially useful since it contains the schema and seed data.
 var databaseServer = builder.AddPostgres("postgres", password: postgresDatabasePassword)
-    .WithDataVolume() // Use a volume instead of a bind so that postgres can modify permissions.
+    .WithDataVolume()
     .WithInitBindMount("../../data/postgres/init");
 
 var assistantDb = databaseServer.AddDatabase("assistant-db");
 
-var vectorDatabase = builder.AddQdrant("vector-db", apiKey: vectorDatabaseApiKey);
+var vectorDatabase = builder.AddQdrant("vector-db", apiKey: vectorDatabaseApiKey)
+    .WithDataVolume();
 
 IResourceBuilder<IResourceWithConnectionString> languageModel;
 
-// Locally we can use a connection string, but in production we use the Azure OpenAI resource so we can deploy through
-// the Azure Developer CLI automatically. Note that we can't set the quota here so you may have to modify things in the
-// portal after provisioning.
 if (builder.ExecutionContext.IsPublishMode)
 {
     var azureLanguageModelResource = builder.AddAzureOpenAI("language-model");
